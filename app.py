72aadef8a04c547292323b7f808b49381ee1ac24
Abiodun Allison <allisonabiodun@gmail.com>
2021-12-19 10:40:40 +0100

    remove unused libraries

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import streamlit as st
 from datetime import timedelta
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
-import seaborn as sns 
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
--- use_functions.py
+++ use_functions.py
@@ -1,8 +1,6 @@
 from datetime import timedelta
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
-import seaborn as sns 
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
